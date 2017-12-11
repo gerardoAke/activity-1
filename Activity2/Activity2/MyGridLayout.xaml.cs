f36c77ee38f4cc04fcb4d1f2b40f89e5784ffec0
Gerardo Ake <gake@ozelot.it>
2017-12-11 18:58:29 -0500

    Activity 2

---
+++ Activity2/Activity2/MyGridLayout.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Activity2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyGridLayout : ContentPage
+    {
+        public MyGridLayout()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Your Text",
+                         entrytext.Text,
+                         "OK");
+        }
+    }
+}
---
+++ Activity2/Activity2/MyLayoutPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Activity2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyLayoutPage : ContentPage
+    {
+        public MyLayoutPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Your Text",
+                         entrytext.Text,
+                         "OK");
+        }
+
+        async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MyGridLayout());
+        }
+    }
+}
